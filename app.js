ae01bfd217e2af8335b20a57b3bc7fcbfdc842b4
AntoineDia <antoine.diambu@gmail.com>
2019-09-08 16:05:08 +0200

    config details

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from 'express'
-import cors from 'cors'
+import express  from 'express'
+import cors     from 'cors'
 import mongoose from 'mongoose'
 
-import pages from './controllers/pages.js'
-import {serv} from './config.js'
+import pages    from './controllers/pages.js'
+import { serv } from './config.js'
 
 mongoose.connect(serv.mongo, {
+  poolSize: 10,
   useCreateIndex: true,
-  useNewUrlParser: true,
-  poolSize: 10
+  useNewUrlParser: true
 })
 
 const app = express()
@@ -16,7 +16,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(cors())
 
-app.get('/', (req, res) => res.end('Wheel'))
+app.get('/', (req, res) => res.end(serv.msg))
 
 app.get('/admin/:operation?', pages.admin)
 
--- config.js
+++ config.js
@@ -1,5 +1,15 @@
 export const serv = {
   port: 2000,
-  msg: 'wheel.io started on 2000',
+  msg: `
+    wheel.io started on 2000
+
+
+    /admin                                   to main page, new operation
+
+    /admin/-operation_id-                    to old operation
+
+    /admin/7921236ccc85214e56e183f03a38b454  to current op
+
+  `,
   mongo: 'mongodb://luckysmart:1aDhqk2jAS0FzbBo@projectslc-shard-00-00-soavx.mongodb.net:27017,projectsls-shard-00-01-soavx.mongodb.net:27017,proectslc-shard-00-02-soavx.mongodb.net:27017/wheel_lck_io?ssl=true&replicaSet=ProjectsLC-shard-0&authSource=admin'
 }
