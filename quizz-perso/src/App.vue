316e764c0a1258383a5b5182b69b04602941a39c
AntoineDia <alfa231297@gmail.com>
2019-09-19 16:16:50 +0200

    vue running

--- quizz-perso/src/App.vue
+++ quizz-perso/src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-
+    What happend no?
+    Welcome to the world
+    Qet up now you're ready
   </div>
 </template>
 
 <script>
-import TextEditor from './components/TextEditor'
-
 export default {
   name: 'app',
-  components: {
-    TextEditor
-  },
   methods: {
-    textEditorUpdate(html){
-      // eslint-disable-next-line
-      console.log(html)
-    },
-
   },
 }
 </script>
 
 <style>
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
